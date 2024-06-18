1dd7388ccc374347b57f6eefec3f02f049640d6f
Udesh Piyumantha <it21013928@my.sliit.lk>
2024-06-18 12:58:10 +0530

    added login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { Button, TextField } from "@mui/material";
+import { UserLogin } from "../services/authService";
+import toast from "react-hot-toast";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await UserLogin({ username: username, password: password })
+      .then(({ data }) => {
+        toast.success("Login successful");
+        localStorage.setItem("token", data.access_token);
+        setTimeout(() => {
+          window.location = "/user-events";
+        }, 500);
+      })
+      .catch(() => {
+        toast.error("Something went wrong")
+      });
+  };
+
+  return (
+    <div style={styles.container}>
+      <form style={styles.form} onSubmit={handleSubmit}>
+        <h2 style={styles.title}>User Login</h2>
+
+        <TextField
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsernameChange}
+          fullWidth
+          required
+          margin="normal"
+        />
+        <TextField
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          fullWidth
+          required
+          margin="normal"
+        />
+        <Button type="submit" style={styles.button}>
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    background: `url('/backg.jpg')`,
+    backgroundSize: "cover",
+    position: "relative",
+    opacity:0.9
+  },
+  gradientBackground: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
+    zIndex: -1,
+    clipPath: "polygon(0 0, 100% 0, 100% 75%, 0 100%)",
+  },
+  form: {
+    backgroundColor: "white",
+    padding: "2rem",
+    borderRadius: "8px",
+    boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+    zIndex: 1,
+  },
+  title: {
+    marginBottom: "1.5rem",
+    color: "#333",
+    textAlign: "center",
+  },
+  button: {
+    display: "block",
+    width: "100%",
+    padding: "0.75rem",
+    borderRadius: "4px",
+    border: "none",
+    backgroundColor: "#667eea",
+    color: "white",
+    fontSize: "1rem",
+    cursor: "pointer",
+    marginTop: "2rem",
+  },
+};
+
+export default LoginPage;
